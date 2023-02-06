6d736bc7a88fb9f2b910703d37ab673f5854a10f
Bishnu Rawal <rawal.bishnu@gmail.com>
2023-02-06 08:37:01 +0545

    More on methods

--- Fundamentals/A6-Methods.cs
+++ Fundamentals/A6-Methods.cs
@@ -17,7 +17,7 @@ class MethodLearner
 
     public void Add(float first, float second)
     {
-        float sum = first + second;
+        float sum = first + second; // Augend + Addend
         string dText = $"{first} plus {second} equals {sum}";     // String interpolation
 
         Console.WriteLine(dText);  //3 + 7 = 10
@@ -42,5 +42,55 @@ class MethodLearner
     {
         float sum = first + second + third;
         return sum;
-    }        
+    }
+
+    // Optional parameters, variable number of arguments (params)
+    public double Multiply(double x, double y, double z = 1)
+    {
+        var product = x * y * z;    // Multiplicand * Multiplier
+        return product;
+    }
+
+    public double SumOfSquares(params double[] numbers)
+    {
+        double sum = 0;
+        foreach(double num in numbers)
+        {
+            sum = sum + num * num;
+        }
+
+        return sum;
+    }
+
+    // Expression bodied memmbers
+    public float Divide(float first, float second) => first / second;
+
+    // Write a method to calulate minimum among supplied numbers.
+    public int CalculateMin(params int[] numbers)   // 4,6,1,8,9,23
+    {
+        int min = numbers[0];
+        foreach(int num in numbers)
+        {
+            if(num < min)
+                min = num;
+        }
+
+        return min;
+    }
+
+    // Returning multiple values
+    public (int, int) CalculateMinMax(params int[] numbers)   // 4,6,1,8,9,23
+    {
+        int min = numbers[0];
+        int max = numbers[0];
+        foreach(int num in numbers)
+        {
+            if(num < min)
+                min = num;
+            if(num > max)
+                max = num;
+        }
+
+        return (min, max);      // Tuple
+    }
 }
--- Fundamentals/EntryPoint.cs
+++ Fundamentals/EntryPoint.cs
@@ -13,14 +13,25 @@ MethodLearner ml = new();
 // ml.Add(3445.3345f, 123.345f);
 // ml.Add(123124.3f, 756.798f);
 
-var s = ml.Greet();
-Console.WriteLine(s);
+// var s = ml.Greet();
+// Console.WriteLine(s);
 
-var d = ml.GetDay();
-Console.WriteLine(d);
+// var d = ml.GetDay();
+// Console.WriteLine(d);
 
-float f = 12.3f, se = 45.6f, t = 67.12f;
-var sum = ml.Add(f, se, t);
-var tt = $"{f} + {se} + {t} = {sum}";
-Console.WriteLine(tt);
+// float f = 12.3f, se = 45.6f, t = 67.12f;
+// var sum = ml.Add(f, se, t);
+// var tt = $"{f} + {se} + {t} = {sum}";
+// Console.WriteLine(tt);
 
+// var p = ml.Multiply(34.5, 67.7, 45.12);
+// var p1 = ml.Multiply(34.5, 67.7);
+
+// var p2 = ml.SumOfSquares(34.5, 3, 4, 5, 6, 7);
+// var p3 = ml.SumOfSquares(34.5, 3, 4, 5, 6, 7, 4, 4, 6, 7, 8, 8, 3, 3, 6, 67, 78);
+
+// // Named arguments
+// var p5 = ml.Multiply(z: 34.5, x: 67.7, y: 45.12);
+
+var x = ml.CalculateMin(3, 4, 5, 6, 7, 8, 12, 98, 2, 45, -1);
+var (min, max) = ml.CalculateMinMax(3, 4, 5, 6, 7, 8, 12, 98, 2, 45, -1);
