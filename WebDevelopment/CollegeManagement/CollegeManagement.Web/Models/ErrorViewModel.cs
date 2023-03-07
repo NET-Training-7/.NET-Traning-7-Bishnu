f0a009e298cb18ed270a35604b47ecb34cf82ce1
Bishnu Rawal <rawal.bishnu@gmail.com>
2023-03-07 12:58:54 +0545

    Start college managment system

---
+++ WebDevelopment/CollegeManagement/CollegeManagement.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CollegeManagement.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WebDevelopment/CollegeManagement/CollegeManagement.Web/Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace CollegeManagement.Web.Models;
+public class Student
+{
+    public int RollNo { get; set; }
+    public string Name { get; set; }
+    public string Major { get; set; }
+    public string Contact { get; set; }
+    public string Semester { get; set; }
+}
