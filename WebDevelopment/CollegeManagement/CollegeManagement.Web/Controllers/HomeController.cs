c93332894eed503edf971b08a09da290f95146e0
Bishnu Rawal <rawal.bishnu@gmail.com>
2023-03-09 08:34:05 +0545

    Learn ADO.NET

--- WebDevelopment/CollegeManagement/CollegeManagement.Web/Controllers/HomeController.cs
+++ WebDevelopment/CollegeManagement/CollegeManagement.Web/Controllers/HomeController.cs
@@ -1,21 +1,43 @@
 ﻿using CollegeManagement.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using System.Diagnostics;
 
 namespace CollegeManagement.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult LearnAdoNET()
+        {
+            // Fetch Student data from student table
+            string connectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=CollegeDb_Test;Integrated Security=True;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand("Select * from dbo.Student", connection);
+            
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+
+            List<Student> students = new List<Student>();
+
+            while (reader.Read())
+            {
+                var student = new Student()
+                {    
+                    RollNo = reader.GetInt32(0),
+                    Name = reader.GetString(1),
+                    Contact = reader.GetString(2),
+                    Major = reader.GetString(3),
+                    Semester = reader.GetString(5),
+                };
+                students.Add(student);
+            }
+
+            return View("ADO", students);
         }
 
         public IActionResult Privacy()
